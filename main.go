89bf06967e3a53361176479da12139e87a33365c
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-11-04 19:20:53 +0330

    complate

--- main.go
+++ main.go
@@ -14,12 +14,54 @@ func main() {
 
 	v := detector.Detector{Input: name}
 
-	// pattern := `^[0-9]+\.[0-9]+$`
-	// ok, err := regexp.MatchString(pattern, name)
+	comma := v.Comma()
+	if comma {
+		fmt.Println("Comma(; or ,) detected")
+	}
 
-	// if err != nil {
-	// 	fmt.Println("Erro", err.Error())
-	// }
+	command := v.Commands()
+	if command {
+		fmt.Println("Command (if else for) detected")
+	}
 
-	// fmt.Println(ok)
+	reserved2 := v.Reserve2()
+	if reserved2 {
+		fmt.Println("Reserved (return or break) detected")
+	}
+
+	reserved := v.Reserved()
+	if reserved {
+		fmt.Println("Reserved (int fload string bool char) detected")
+	}
+
+	operators := v.Operators()
+	if operators {
+		fmt.Println("Operators (> < = <= >= == !=) detected")
+	}
+
+	bracket := v.Bracketes()
+	if bracket {
+		fmt.Println("Bracket ( } ) { ( ) detected")
+	}
+
+	Qnumber := v.QNumber()
+	if Qnumber {
+		fmt.Println("decimal number detected")
+	}
+
+	Znumber := v.ZNumber()
+	if Znumber {
+		fmt.Println("integer number detected")
+	}
+
+	variable := v.Variable()
+	if variable {
+		fmt.Println("Variable detected")
+	}
+
+	string := v.Strings()
+	if string {
+		fmt.Println("String detected")
+	}
+
 }
