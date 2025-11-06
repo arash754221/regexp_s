4b3339ac1dfeb2e292d96922edfe07494432a5fc
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-11-06 08:21:28 +0330

    adding

--- main.go
+++ main.go
@@ -2,48 +2,51 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+	"strings"
 )
 
 func main() {
 
-	Patterns := map[string]string{
-		"^[a-zA-Z_][a-zA-Z_0-9]*$":               "variable",
-		"^[0-9]+$":                               "integer",
-		`^[0-9]+\.[0-9]+$`:                       "float",
-		`^".*"$`:                                 "string",
-		"^[><=!][=]{0,1}$":                       "operator",
-		"(^if$|^else$|^for$)":                    "keyword",
-		"(^int$|^float$|^string$|^bool$|^char$)": "type",
-		"^[(){}]{1}$":                            "bracket",
-		"(^return$|^break$)":                     "control",
-		"^[;,]{1}$":                              "separator",
+	// Patterns := map[string]string{
+	// 	"^[a-zA-Z_][a-zA-Z_0-9]*$":               "variable",
+	// 	"^[0-9]+$":                               "integer",
+	// 	`^[0-9]+\.[0-9]+$`:                       "float",
+	// 	`^".*"$`:                                 "string",
+	// 	"^[><=!][=]{0,1}$":                       "operator",
+	// 	"(^if$|^else$|^for$)":                    "keyword",
+	// 	"(^int$|^float$|^string$|^bool$|^char$)": "type",
+	// 	"^[(){}]{1}$":                            "bracket",
+	// 	"(^return$|^break$)":                     "control",
+	// 	"^[;,]{1}$":                              "separator",
+	// }
+
+	var query string
+
+	fmt.Print("Enter Programm: ")
+	fmt.Scan(&query)
+
+	str := strings.Split(query, ``)
+	for i, o := range str {
+
+		fmt.Println(i, o)
 	}
+	// y := false
+	// for pat, msg := range Patterns {
+	// 	ok, err := regexp.MatchString(pat, input)
+	// 	if err != nil {
+	// 		fmt.Println("🚫 regexp Error")
+	// 	} else {
+	// 		if ok {
+	// 			fmt.Println("✅ ", msg)
+	// 			y = true
+	// 			break
 
-	for {
-		var input string
+	// 		}
+	// 	}
+	// }
 
-		fmt.Print("Enter your input and i guess: ")
-		fmt.Scan(&input)
-
-		y := false
-		for pat, msg := range Patterns {
-			ok, err := regexp.MatchString(pat, input)
-			if err != nil {
-				fmt.Println("🚫 regexp Error")
-			} else {
-				if ok {
-					fmt.Println("✅ ", msg)
-					y = true
-					break
-
-				}
-			}
-		}
-
-		if !y {
-			fmt.Printf("‼️ Oops what is this? %s\n", input)
-		}
-	}
+	// if !y {
+	// 	fmt.Printf("‼️ Oops what is this? %s\n", input)
+	// }
 
 }
